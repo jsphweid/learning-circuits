17b68ab862cde474ffb471da29540863c5869961
jsphweid <jsphweid@gmail.com>
2021-06-20 12:27:13 -0500

    add more tests

--- tests/test_relay.py
+++ tests/test_relay.py
@@ -1,5 +1,14 @@
 from circuit.relay import Relay
 
+
+def test_relay():
+	relay = Relay(power=True)
+
+	assert relay.output_state == False
+	relay.switch_on()
+	assert relay.output_state == True
+
+
 def test_relay_as_repeater():
 	relay_one = Relay(power=True)
 	relay_two = Relay(power=True, switch=relay_one.output)
@@ -11,4 +20,21 @@ def test_relay_as_repeater():
 
 	assert relay_one.output_state == True
 	assert relay_two.output_state == True
+
+
+def test_relay_as_repeater_but_more_complicated():
+	relay_one = Relay(power=True)
+	relay_two = Relay(power=True, switch=relay_one.output)
+	relay_three = Relay(power=True, switch=relay_two.output)
+
+	assert relay_one.output_state == False
+	assert relay_two.output_state == False
+	assert relay_three.output_state == False
+
+	relay_one.switch_on()
+
+	assert relay_one.output_state == True
+	assert relay_two.output_state == True
+	assert relay_three.output_state == True
+
 
