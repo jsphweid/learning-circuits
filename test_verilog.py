1b66cadb4e3fc3812e8e3acd39daaf69712f16dc
jsphweid <jsphweid@gmail.com>
2021-06-27 17:01:30 -0500

    use pytest as test runner

--- test_verilog.py
+++ test_verilog.py
@@ -6,8 +6,7 @@ import shutil
 from subprocess import check_output, CalledProcessError
 from typing import List, NamedTuple
 
-
-class TestFile(NamedTuple):
+class FileToTest(NamedTuple):
 	relative_path: str
 	filename: str
 
@@ -19,44 +18,52 @@ def ensure_build_folder_is_reset():
 	os.makedirs(build_dir)
 
 
-def get_test_files() -> List[TestFile]:
+def get_test_files() -> List[FileToTest]:
 	relevant_paths = []
 	for path, _, files in os.walk("./verilog"):
 		for name in files:
 			if "_test.sv" in name:
 				relevant_paths.append(
-					TestFile(os.path.join(path, name), name))
+					FileToTest(os.path.join(path, name), name))
 	return relevant_paths
 
 
-def run_test_file(test_file: TestFile):
-	# compile
+class CompilationException(Exception):
+	def __init__(self, *args: object) -> None:
+			super().__init__(*args)
+	pass
+
+
+def compile_test_file(test_file: FileToTest) -> str:
 	output_path = f"./build/{test_file.filename}.vvp"
 	compile_command = f"iverilog -grelative-include -g2012 -o {output_path} {test_file.relative_path}"
 
 	try:
-		what = check_output(compile_command, shell=True)
+		check_output(compile_command, shell=True)
+		return output_path
 	except CalledProcessError as e:
-		print(f"Could not compile verilog test {test_file.relative_path}", e)
-		sys.exit(1)
+		raise CompilationException(e)
 
-	# simulate
-	simulation_command = ["vvp", output_path]
+
+def assert_no_errors(compiled_filepath: str):
+	simulation_command = ["vvp", compiled_filepath]
 
 	try:
 		output = check_output(simulation_command).decode("utf-8")
 		assertion_errors = len(re.findall(r'(Error: |ERROR: )', output))
-		print(output)
-		# color = Fore.YELLOW if assertion_errors > 0 else Fore.GREEN
-		print("Found " + str(assertion_errors) + " assertion errors in " + test_file.filename)
-		# return {"assertion_errors": assertion_errors, "run_errors": 0}
+		if assertion_errors > 0:
+			raise Exception(output)
 	except CalledProcessError as e:
-		print(f"Could not run verilog test {output_path}", e)
+		print(f"Could not assert no errors in {compiled_filepath}", e)
 		sys.exit(1)
 
 
-if __name__ == "__main__":
+@pytest.fixture(autouse=True, scope="module")
+def setup():
 	ensure_build_folder_is_reset()
-	test_files = get_test_files()
-	for test_file in test_files:
-		run_test_file(test_file)
+
+
+@pytest.mark.parametrize('file', get_test_files())
+def test_file(file):
+	compiled_filepath = compile_test_file(file)
+	assert_no_errors(compiled_filepath)
