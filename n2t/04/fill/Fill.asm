2ded08d21ccbc8c50dc798eed3fe33b3a3d0ab76
jsphweid <jsphweid@gmail.com>
2021-07-03 22:26:28 -0500

    complete fill

--- n2t/04/fill/Fill.asm
+++ n2t/04/fill/Fill.asm
@@ -12,3 +12,95 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+// R0 stores current white
+// R1 stores current black
+// R2 is max
+
+@KBD
+D=A
+
+// FINAL
+@R2
+M=D
+
+// TEMP -- just to test with smaller portions since a refresh takes an eternity on the CPU Emulator
+// @SCREEN
+// D=A
+// @R2
+// M=D+1
+// @R2
+// M=M+1
+
+
+(WHITE)
+  @R2 // if we've reached kbd, go back to eval loop
+  D=M
+  @R0
+  D=M-D
+  @EVAL
+  D;JEQ
+
+  @R0 // paint
+  A=M
+  M=0
+
+  @R0 // increment
+  M=M+1
+  
+  @WHITE
+  0;JMP
+
+
+(BLACK)
+  @R2 // if we've reached kbd, go back to eval loop
+  D=M
+  @R1
+  D=M-D
+  @EVAL
+  D;JEQ
+
+  @R1 // paint
+  A=M
+  M=-1
+
+  @R1 // increment
+  M=M+1
+
+  @BLACK
+  0;JMP
+
+
+(PREPWHITE)
+  @SCREEN
+  D=A
+  @R0
+  M=D
+
+  @WHITE
+  0;JMP
+
+
+(PREPBLACK)
+  @SCREEN
+  D=A
+  @R1
+  M=D
+
+  @BLACK
+  0;JMP
+
+
+
+(EVAL)
+  @KBD // store keyboard value in D
+  D=M
+
+  @PREPBLACK
+  D;JGT
+
+  @PREPWHITE
+  0;JMP
+
+  // @LOOP
+  // 0;JMP
