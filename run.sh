fdf358c81acd3d089d0ecff2b4ee3e5b2cd461bd
jsphweid <jsphweid@gmail.com>
2021-07-05 10:43:44 -0500

    progress maybe

---
+++ run.sh
@@ -0,0 +1,12 @@
+cd n2t/verilog
+rm -rf obj_dir
+ls -la
+# verilator -Wall --cc my_computer.sv -I./ --exe ../../computer.cpp -LDFLAGS "-lmingw32 -lSDL2main -lSDL2"
+
+verilator -Wall --cc ./n2t/verilog/my_computer.sv -I./n2t/verilog --exe computer.cpp -LDFLAGS "-lmingw32 -lSDL2main -lSDL2"
+
+# cd obj_dir
+# make -f Vcomputer.mk
+# cd ../
+# ./05/obj_dir/Vcomputer.exe
+
